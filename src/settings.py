7195c4690b7f01a04b2f172416aeded0ee0b097f
Artsiom_PH <workartpharm@gmail.com>
2023-12-26 15:00:32 +0300

    replace key

--- src/settings.py
+++ src/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-_&nr6qr@9a-w%#!#2_mv&@a1c*n%=ucc@9gklq@i#ah77$5e4s"
-)
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
